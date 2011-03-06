c88ec01534985bea617377a589b65f9408ec9e4f
trh178 <trh178@gmail.com>
2011-03-06 15:55:15 -0500

    Initial commit of shanes project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shanes::Application.config.secret_token = '0f235472f79db341f2dda8fa0faa9187f5e2d7ac917b344d04d46c85cea5b9105f30db0544cba8485add840f3d6317a00517d42cfdde1aa32d42f09880a1b632'
